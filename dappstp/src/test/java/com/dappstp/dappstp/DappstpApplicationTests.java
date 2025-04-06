508ef721b84a5443366f7f29508f71afdb1f90d4
MatiasCionci <cioncimatias2@gmail.com>
2025-04-06 03:33:35 -0300

    Initial

---
+++ dappstp/src/test/java/com/dappstp/dappstp/DappstpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dappstp.dappstp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DappstpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
