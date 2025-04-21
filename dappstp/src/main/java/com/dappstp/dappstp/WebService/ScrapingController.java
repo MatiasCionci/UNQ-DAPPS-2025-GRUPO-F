ea37153fce58b8c8db1905fbb50694713b50457a
MatiasCionci <cioncimatias2@gmail.com>
2025-04-21 18:13:14 -0300

    Pulliendo

--- dappstp/src/main/java/com/dappstp/dappstp/WebService/ScrapingController.java
+++ dappstp/src/main/java/com/dappstp/dappstp/WebService/ScrapingController.java
@@ -1,33 +1,13 @@
 package com.dappstp.dappstp.WebService;
 
 import java.util.List;
-import java.util.Map; // Importar Map
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus; // Importar HttpStatus
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping; // Importar PostMapping (alternativa)
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-// Quita esta importación si no la usas directamente aquí
-// import com.dappstp.dappstp.model.Player;
 import com.dappstp.dappstp.model.PlayerBarcelona;
-// Quita esta importación si no la usas directamente aquí
-// import com.dappstp.dappstp.model.PlayerProfileScraping;
-import com.dappstp.dappstp.service.Scraping.PlayerProfileScrapingService;
 import com.dappstp.dappstp.service.Scraping.ScraperServicePlayers;
 
-import org.springframework.web.bind.annotation.PathVariable; // Para leer el nombre del equipo de la URL
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter; // Para describir el parámetro en Swagger
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/api") // Ruta base para todos los endpoints de esta API
@@ -35,15 +15,12 @@ public class ScrapingController {
     @Autowired
     private final ScraperServicePlayers scraperService;
     
-    private final PlayerProfileScrapingService playerProfileService;
+ 
 
     // Inyección de dependencias vía constructor para ambos servicios
     @Autowired
-    public ScrapingController(ScraperServicePlayers scraperServicePlayers,
-                              PlayerProfileScrapingService playerProfileService,
-                              ScraperServicePlayers scraperService) {
+    public ScrapingController(ScraperServicePlayers scraperService) {
  
-        this.playerProfileService = playerProfileService;
         this.scraperService = scraperService;
     }
 
