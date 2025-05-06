a5b704901d70d339790cc948244b647a283eb91d
MatiasCionci <110140323+MatiasCionci@users.noreply.github.com>
2025-05-06 00:41:17 -0300

    Update ScraperServicePlayers.java

--- dappstp/src/main/java/com/dappstp/dappstp/service/Scraping/ScraperServicePlayers.java
+++ dappstp/src/main/java/com/dappstp/dappstp/service/Scraping/ScraperServicePlayers.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.util.*;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Service
